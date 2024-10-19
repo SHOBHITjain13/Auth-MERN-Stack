24ae92ead9c22092ab19f54de141189d08d036a5
ShobhitJain <103408720+SHOBHITjain13@users.noreply.github.com>
2024-10-19 18:13:46 +0530

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { handleError, handleSuccess } from "../utils";
-import { ToastContainer } from "react-toastify";
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { handleError, handleSuccess } from '../utils';
+import { ToastContainer } from 'react-toastify';
 
 function Home() {
-    const [loggedInUser, setLoggendInUser] = useState('');
+    const [loggedInUser, setLoggedInUser] = useState('');
     const [products, setProducts] = useState('');
     const navigate = useNavigate();
+    useEffect(() => {
+        setLoggedInUser(localStorage.getItem('loggedInUser'))
+    }, [])
 
-    useEffect(()=> {
-        setLoggendInUser(localStorage.getItem('loggedInUser'))
-    }, []);
-
-    const handleLogout = (e)=> {
+    const handleLogout = (e) => {
         localStorage.removeItem('token');
         localStorage.removeItem('loggedInUser');
-        handleSuccess('User Loggedout')
-        setTimeout(()=> {
-            navigate('/login')
+        handleSuccess('User Loggedout');
+        setTimeout(() => {
+            navigate('/login');
         }, 1000)
     }
-    const fetchProduct = async() => {
-        try{
-            const url = "https://auth-mern-stack-api.vercel.app/products";
+
+    const fetchProducts = async () => {
+        try {
+            const url = "https://deploy-mern-app-1-api.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
@@ -32,29 +32,30 @@ function Home() {
             const result = await response.json();
             console.log(result);
             setProducts(result);
-        }catch(err){
-            handleError(err)
+        } catch (err) {
+            handleError(err);
         }
     }
-    useEffect(()=> {
-        fetchProduct()
+    useEffect(() => {
+        fetchProducts()
     }, [])
+
     return (
         <div>
-        <h1>{loggedInUser}</h1>
-        <button onClick={handleLogout}>Logout</button>
-        <div>
-            {
-                products && products?.map((item, index) => (
-                    <ul key={index}>
-                        <span>{item.name} : {item.price} </span>
-                    </ul>
-                ))
-            }
-        </div>
-        <ToastContainer/>
+            <h1>Welcome {loggedInUser}</h1>
+            <button onClick={handleLogout}>Logout</button>
+            <div>
+                {
+                    products && products?.map((item, index) => (
+                        <ul key={index}>
+                            <span>{item.name} : {item.price}</span>
+                        </ul>
+                    ))
+                }
+            </div>
+            <ToastContainer />
         </div>
     )
 }
 
-export default Home;
+export default Home
