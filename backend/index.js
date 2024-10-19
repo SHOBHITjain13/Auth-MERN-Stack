c39ee9c16116264e514c864fc20a199534cdfd74
SHOBHITjain13 <Shobbitjain13@gmail.com>
2024-10-19 17:37:23 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const cors = require('cors');
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const authRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
+
+require('dotenv').config();
+require('./Models/db');
+const PORT = process.env.PORT || 8000;
+
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/auth', authRouter);
+app.use('/products', ProductRouter);
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+})
