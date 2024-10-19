e7207bc94dcffcb46468c3157caaa1912b2544ec
ShobhitJain <103408720+SHOBHITjain13@users.noreply.github.com>
2024-10-19 18:15:13 +0530

    Update signup.js

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
-import { handleError, handleSuccess } from "../utils";
+import { handleError, handleSuccess } from '../utils';
 
 function Signup() {
 
@@ -9,9 +10,9 @@ function Signup() {
         name: '',
         email: '',
         password: ''
-    });
-    const navigate = useNavigate();
+    })
 
+    const navigate = useNavigate();
     const handleChange = (e) => {
         const { name, value } = e.target;
         console.log(name, value);
@@ -23,11 +24,11 @@ function Signup() {
     const handleSignup = async (e) => {
         e.preventDefault();
         const { name, email, password } = signupInfo;
-        
         if (!name || !email || !password) {
-            return handleError('name, email and password are required');
-        } try {
-            const url = 'https://auth-mern-stack-api.vercel.app/auth/signup';
+            return handleError('name, email and password are required')
+        }
+        try {
+            const url = `https://deploy-mern-app-1-api.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -36,55 +37,60 @@ function Signup() {
                 body: JSON.stringify(signupInfo)
             });
             const result = await response.json();
-            const {success, message, error} = result;
-            if(success){
+            const { success, message, error } = result;
+            if (success) {
                 handleSuccess(message);
                 setTimeout(() => {
                     navigate('/login')
                 }, 1000)
-            }else if(error){
+            } else if (error) {
                 const details = error?.details[0].message;
                 handleError(details);
-            }else if(!success){
+            } else if (!success) {
                 handleError(message);
             }
+            console.log(result);
         } catch (err) {
             handleError(err);
         }
     }
-
     return (
-        <div className="container">
+        <div className='container'>
             <h1>Signup</h1>
             <form onSubmit={handleSignup}>
                 <div>
-                    <label htmlFor="name">Name</label>
+                    <label htmlFor='name'>Name</label>
                     <input
                         onChange={handleChange}
-                        type="text"
-                        name="name"
+                        type='text'
+                        name='name'
                         autoFocus
-                        placeholder="Enter your name..."
-                        value={signupInfo.name} />
+                        placeholder='Enter your name...'
+                        value={signupInfo.name}
+                    />
                 </div>
                 <div>
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor='email'>Email</label>
                     <input
-                        onChange={handleChange} type="email"
-                        name="email"
-                        placeholder="Enter your Email..."
-                        value={signupInfo.email} />
+                        onChange={handleChange}
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email...'
+                        value={signupInfo.email}
+                    />
                 </div>
                 <div>
-                    <label htmlFor="password">Password</label>
+                    <label htmlFor='password'>Password</label>
                     <input
-                        onChange={handleChange} type="password"
-                        name="password"
-                        placeholder="Enter your Password..."
-                        value={signupInfo.password} />
+                        onChange={handleChange}
+                        type='password'
+                        name='password'
+                        placeholder='Enter your password...'
+                        value={signupInfo.password}
+                    />
                 </div>
-                <button>Signup</button>
-                <span>Already have an account ? &nbsp;
+                <button type='submit'>Signup</button>
+                <span>Already have an account ?
                     <Link to="/login">Login</Link>
                 </span>
             </form>
@@ -93,4 +99,4 @@ function Signup() {
     )
 }
 
-export default Signup;
+export default Signup
