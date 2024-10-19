1359ec2a5d54835889234b55ffcbe68449e629ed
ShobhitJain <103408720+SHOBHITjain13@users.noreply.github.com>
2024-10-19 18:14:46 +0530

    Update login.js

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
-import { handleError, handleSuccess } from "../utils";
+import { handleError, handleSuccess } from '../utils';
 
 function Login() {
 
     const [loginInfo, setLoginInfo] = useState({
         email: '',
         password: ''
-    });
+    })
+
     const navigate = useNavigate();
 
     const handleChange = (e) => {
@@ -22,11 +24,11 @@ function Login() {
     const handleLogin = async (e) => {
         e.preventDefault();
         const { email, password } = loginInfo;
-        
-        if ( !email || !password) {
-            return handleError(' email and password are required');
-        } try {
-            const url = 'https://auth-mern-stack-api.vercel.app/auth/login';
+        if (!email || !password) {
+            return handleError('email and password are required')
+        }
+        try {
+            const url = `https://deploy-mern-app-1-api.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
@@ -35,47 +37,52 @@ function Login() {
                 body: JSON.stringify(loginInfo)
             });
             const result = await response.json();
-            const {success, message, jwtToken, name, error} = result;
-            if(success){
+            const { success, message, jwtToken, name, error } = result;
+            if (success) {
                 handleSuccess(message);
                 localStorage.setItem('token', jwtToken);
-                localStorage.setItem('loggedInUser', name)
+                localStorage.setItem('loggedInUser', name);
                 setTimeout(() => {
                     navigate('/home')
                 }, 1000)
-            }else if(error){
+            } else if (error) {
                 const details = error?.details[0].message;
                 handleError(details);
-            }else if(!success){
+            } else if (!success) {
                 handleError(message);
             }
+            console.log(result);
         } catch (err) {
             handleError(err);
         }
     }
 
     return (
-        <div className="container">
+        <div className='container'>
             <h1>Login</h1>
             <form onSubmit={handleLogin}>
                 <div>
-                    <label htmlFor="email">Email</label>
+                    <label htmlFor='email'>Email</label>
                     <input
-                        onChange={handleChange} type="email"
-                        name="email"
-                        placeholder="Enter your Email..."
-                        value={loginInfo.email} />
+                        onChange={handleChange}
+                        type='email'
+                        name='email'
+                        placeholder='Enter your email...'
+                        value={loginInfo.email}
+                    />
                 </div>
                 <div>
-                    <label htmlFor="password">Password</label>
+                    <label htmlFor='password'>Password</label>
                     <input
-                        onChange={handleChange} type="password"
-                        name="password"
-                        placeholder="Enter your Password..."
-                        value={loginInfo.password} />
+                        onChange={handleChange}
+                        type='password'
+                        name='password'
+                        placeholder='Enter your password...'
+                        value={loginInfo.password}
+                    />
                 </div>
-                <button>Login</button>
-                <span>Dont have an account ? &nbsp;
+                <button type='submit'>Login</button>
+                <span>Does't have an account ?
                     <Link to="/signup">Signup</Link>
                 </span>
             </form>
@@ -84,4 +91,4 @@ function Login() {
     )
 }
 
-export default Login;
+export default Login
